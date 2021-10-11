ede8fca61ac3e9f278cc5ffa724c0721020045d4
Collembole <savinien.monteil@labo-ve.fr>
2021-10-11 10:38:08 +0200

    Initialisation du projet et test de la clé d'API :tada:

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const getPopularMovies = async () => {
+  const result = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=153bac550dbfc9bda1b2f5ef2f99a808");
+  console.log(JSON.stringify(result.data.results[0], null, 2));
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  getPopularMovies()
+  return( <View></View> )
+};
+
+export  default App;
