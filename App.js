9ee7d719ec89c44d131cd01a5849812e75b1af97
Collembole <savinien.monteil@labo-ve.fr>
2021-10-11 10:53:34 +0200

    display infos movie from the API :tada:

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import axios from 'axios';
 
 const getPopularMovies = async () => {
   const result = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=153bac550dbfc9bda1b2f5ef2f99a808");
-  console.log(JSON.stringify(result.data.results[0], null, 2));
+  return result.data.results;
 }
 
 const App = () => {
-  getPopularMovies()
-  return( <View></View> )
+  const [movie, setMovie] = useState('')
+  getPopularMovies().then((movies) => {
+    setMovie(movies[0]);
+  });
+  return( 
+  <View>
+    <Text> Nom du film : {movie.original_title}</Text>
+    <Text> Langue : {movie.original_language}</Text>
+    <Text> Date de sortie : {movie.release_date}</Text>
+  </View> 
+  )
 };
 
 export  default App;
